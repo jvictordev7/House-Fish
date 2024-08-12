36fcefadd37ca76d8669b6adf8591f60b76b268d
João Victor <156962782+jvictordev7@users.noreply.github.com>
2024-08-12 16:53:13 -0300

    Finalizei Telas
    Finalizei tela de cadastro e redefinir senha.

--- src/screen/TelaCadastro.js
+++ src/screen/TelaCadastro.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Image, ImageBackground } from 'react-native';
 import Estilos from '../Estilos';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../src/Services/FirebaseConfig'; // Certifique-se do caminho correto
 
 export default function TelaCadastro({ navigation }) {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleRegister = () => {
+        setErrorMessage(''); // Limpar mensagem de erro antes de tentar registro
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Registro bem-sucedido
+                const user = userCredential.user;
+                console.log(user);
+                // Navegar para a tela Home após o registro bem-sucedido
+                navigation.navigate('Home');
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+                
+                // Atualizar mensagem de erro com base no código de erro
+                switch (errorCode) {
+                    case 'auth/invalid-email':
+                        setErrorMessage('O e-mail inserido é inválido.');
+                        break;
+                    case 'auth/email-already-in-use':
+                        setErrorMessage('O e-mail já está em uso.');
+                        break;
+                    case 'auth/weak-password':
+                        setErrorMessage('A senha deve ter pelo menos 6 caracteres.');
+                        break;
+                    default:
+                        setErrorMessage('Ocorreu um erro. Por favor, tente novamente.');
+                }
+            });
+    };
+
     return (
         <View style={{ flex: 1 }}>
             <ImageBackground
@@ -13,11 +52,37 @@ export default function TelaCadastro({ navigation }) {
             <View style={Estilos.container}>
                 <Image source={require('../../assets/logo.png')} style={Estilos.logo} />
                 <Text style={Estilos.loginText}>Cadastro</Text>
-                <TextInput placeholder="Nome" style={Estilos.input} />
-                <TextInput placeholder="Email" style={Estilos.input} keyboardType="email-address" />
-                <TextInput placeholder="Senha" style={Estilos.input} secureTextEntry />
-                <TouchableOpacity style={Estilos.button} onPress={() => navigation.navigate('Home')}>
+                
+                {/* Exibir mensagem de erro, se houver */}
+                {errorMessage ? <Text style={Estilos.errorText}>{errorMessage}</Text> : null}
+                
+                <TextInput 
+                    placeholder="Nome" 
+                    style={Estilos.input} 
+                    value={name}
+                    onChangeText={setName}
+                />
+                <TextInput 
+                    placeholder="Email" 
+                    style={Estilos.input} 
+                    keyboardType="email-address" 
+                    value={email}
+                    onChangeText={setEmail}
+                />
+                <TextInput 
+                    placeholder="Senha" 
+                    style={Estilos.input} 
+                    secureTextEntry 
+                    value={password}
+                    onChangeText={setPassword}
+                />
+                
+                <TouchableOpacity style={Estilos.button} onPress={handleRegister}>
                     <Text style={Estilos.buttonText}>Cadastrar</Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                    
                 </TouchableOpacity>
             </View>
         </View>
--- src/screen/TelaRedefinirSenha.js
+++ src/screen/TelaRedefinirSenha.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Image, ImageBackground } from 'react-native';
 import Estilos from '../Estilos';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../../src/Services/FirebaseConfig'; // Certifique-se do caminho correto
 
 export default function TelaRedefinirSenha({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState(''); // Estado para a mensagem de sucesso ou erro
+
+    const handlePasswordReset = () => {
+        setMessage(''); // Limpar mensagem antes de tentar enviar o e-mail
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                // E-mail de redefinição enviado com sucesso
+                setMessage('E-mail de redefinição enviado. Verifique sua caixa de entrada.');
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+                
+                // Atualizar mensagem de erro com base no código de erro
+                switch (errorCode) {
+                    case 'auth/invalid-email':
+                        setMessage('O e-mail inserido é inválido.');
+                        break;
+                    case 'auth/user-not-found':
+                        setMessage('Nenhum usuário encontrado com esse e-mail.');
+                        break;
+                    default:
+                        setMessage('Ocorreu um erro. Por favor, tente novamente.');
+                }
+            });
+    };
+
     return (
         <View style={{ flex: 1 }}>
             <ImageBackground
@@ -13,10 +44,24 @@ export default function TelaRedefinirSenha({ navigation }) {
             <View style={Estilos.container}>
                 <Image source={require('../../assets/logo.png')} style={Estilos.logo} />
                 <Text style={Estilos.loginText}>Redefinir Senha</Text>
-                <TextInput placeholder="Email" style={Estilos.input} keyboardType="email-address" />
                 
-                <TouchableOpacity style={Estilos.button} onPress={() => navigation.navigate('Login')}>
+                {/* Exibir mensagem de sucesso ou erro */}
+                {message ? <Text style={Estilos.errorText}>{message}</Text> : null}
+                
+                <TextInput 
+                    placeholder="Email" 
+                    style={Estilos.input} 
+                    keyboardType="email-address" 
+                    value={email}
+                    onChangeText={setEmail}
+                />
+                
+                <TouchableOpacity style={Estilos.button} onPress={handlePasswordReset}>
                     <Text style={Estilos.buttonText}>Enviar</Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                
                 </TouchableOpacity>
             </View>
         </View>
